3572b41b891dc015445b6dce017432e3b261dc71
Ed Sweeney <ed@onextent.com>
2021-12-02 07:16:34 -0800

    day2

--- src/day2.rs
+++ src/day2.rs
@@ -25,11 +25,44 @@ pub fn part_a() {
     println!("day2 part a: product {}", pos * depth);
 }
 
+pub fn part_b() {
+    // product should be 1857958050
+    let mut depth: u32 = 0;
+    let mut pos: u32 = 0;
+    let mut aim: u32 = 0;
+
+    let mut update = |val: String| {
+        let split = val.split_once(" ");
+        let (cmd, amt_str) = split.unwrap();
+        let amt: u32 = amt_str.parse::<u32>().unwrap();
+        match cmd {
+            "down" => aim = aim + amt,
+            "up" => aim = aim - amt,
+            _ => {
+                pos = pos + amt;
+                depth = depth + aim * amt;
+            }
+        }
+    };
+
+    if let Ok(lines) = read_lines("data/day2_input.txt") {
+        for line in lines {
+            if let Ok(val) = line {
+                update(val);
+            }
+        }
+    }
+
+    println!("day2 part b: horizontal pos {}", pos);
+    println!("day2 part b: depth {}", depth);
+    println!("day2 part b: product {}", pos * depth);
+}
+
 pub fn run() {
     println!("------------");
     part_a();
     println!("............");
-    //part_b();
+    part_b();
     println!("------------");
 }
 
